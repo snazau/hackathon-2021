c8aaf2ecfdfe699b3f4a286bfe08155165f979fc
snazau <30793581+snazau@users.noreply.github.com>
2021-08-21 20:17:51 +0200

    Add api part

---
+++ api/SQLiteHelper.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sqlite3
+import datetime
+from threading import Lock
+
+__version__ = "0.1.0"
+
+list_hs_ids = """
+    SELECT *
+    FROM hs
+"""
+
+list_transactions_by_id = """
+    SELECT *
+    FROM hs
+    WHERE HsId = QUERY_NAME;
+"""
+#
+
+lock = Lock()
+
+
+class SQLiteHelper:
+    def __init__(self, db='app.db'):
+        self.db = db
+
+    def get_time(self):
+        return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def list_hs_ids(self):
+        lock.acquire()
+        try:
+            conn = sqlite3.connect(self.db)
+            cursor = conn.cursor()
+            cursor.execute(list_hs_ids)
+
+            rows = cursor.fetchall()
+
+            ids = list(set([row[0] for row in rows]))
+            return ids
+
+        finally:
+            lock.release()
+
+    def get_transactions_by_id(self, transaction_id):
+        lock.acquire()
+        try:
+            conn = sqlite3.connect(self.db)
+            cursor = conn.cursor()
+            cursor.execute(list_transactions_by_id.replace("QUERY_NAME", '"' + str(transaction_id) + '"'))
+
+            rows = cursor.fetchall()
+            transaction_ids = list(set([row[1] for row in rows]))
+
+            return transaction_ids
+
+        finally:
+            lock.release()
+
+
+if __name__ == '__main__':
+    db_helper = SQLiteHelper(db="./app.db")
+    ids = db_helper.list_hs_ids()
+    transactions = db_helper.get_transactions_by_id(ids[0])
+    print("ids", ids)
+    print("transactions", transactions)
---
+++ api/server.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+import http
+import json
+import os
+from SQLiteHelper import SQLiteHelper
+
+debug = True
+app = Flask(__name__)
+db_path = os.path.join(os.path.dirname(__file__), "app.db")
+db_helper = SQLiteHelper(db_path)
+
+
+def make_json_response(data, code):
+    if isinstance(data, str):
+        response = app.response_class(response=data, status=code, mimetype="application/json")
+    else:
+        response = app.response_class(response=json.dumps(data), status=code, mimetype="application/json")
+    return response
+
+
+@app.route("/requests", methods=['GET'])
+def process_request():
+    hs_ids = db_helper.list_hs_ids()
+    data = {
+        "hs_ids": hs_ids,
+    }
+    response = make_json_response(data, http.HTTPStatus.OK)
+    return response
+
+
+@app.route("/requests/<guid>", methods=['GET'])
+def get_results(guid):
+    transactions = db_helper.get_transactions_by_id(guid)
+    data = {
+        "transactions": transactions,
+    }
+
+    response = make_json_response(data, http.HTTPStatus.OK)
+    return response
+
+
+def create_app():
+    return app
+
+
+if __name__ == '__main__':
+    if debug:
+        app.debug = debug
+        app.run(host='127.0.0.1', port=7777)
+    else:
+        app.run()
